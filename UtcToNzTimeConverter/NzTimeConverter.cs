0fc3655b236ffd3b9adc79634e30d2111a30cc1b
JNing73 <94809226+JNing73@users.noreply.github.com>
2025-01-28 10:16:45 +1300

    Create test cases for UTC time offset strings

--- UtcToNzTimeConverter/NzTimeConverter.cs
+++ UtcToNzTimeConverter/NzTimeConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace UtcToNzTimeConverter;
 public class NzTimeConverter
@@ -13,9 +14,16 @@ public class NzTimeConverter
             timestamp.Length > 0 &&
             timestamp.Substring(timestamp.Length - 1) == "Z"
             );
+
+        bool mayBeOffset = IsUtcOffset(timestamp);
+
         if (mayBeZulu)
         {
             format += "Z";
+        }
+        else if (mayBeOffset)
+        {
+            format += "K";
         }
 
         bool validTimeStamp = DateTime.TryParseExact(
@@ -26,7 +34,7 @@ public class NzTimeConverter
                 out DateTime dt
                 );
 
-        if (validTimeStamp && mayBeZulu)
+        if (validTimeStamp && (mayBeZulu || mayBeOffset))
         {
             return dt.ToString(_standardFormat);
         }
@@ -41,5 +49,11 @@ public class NzTimeConverter
                 $"for formatting errors or invalid dates and/or times"
                 );
         }
+    }
+
+    public static bool IsUtcOffset(string timestamp)
+    {
+        string pattern = @"[+-]\d{2}:\d{2}$";
+        return Regex.IsMatch(timestamp, pattern);
     }
 }
